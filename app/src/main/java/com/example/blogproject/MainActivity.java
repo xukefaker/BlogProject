731b127bd77a114199fb74f0f3d1fe02a8daa9d3
joyzhouyi <2643983350@qq.com>
2020-08-11 16:31:32 +0800

    我就点击
    没什么

--- app/src/main/java/com/example/blogproject/MainActivity.java
+++ app/src/main/java/com/example/blogproject/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.blogproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"woshizhouyi",Toast.LENGTH_LONG).show();
+
+
+
+            }
+        });
     }
 }
