697a1925b0e1fec225d7a68aaf438a582b94d8a2
AshHero <57551516+xukefaker@users.noreply.github.com>
2020-08-11 16:09:11 +0800

    faker测试

---
+++ app/src/main/java/com/example/blogproject/FakerTest.java
@@ -0,0 +1,14 @@
+package com.example.blogproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FakerTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faker_test);
+    }
+}
