dbc2c043e8cc2a93cf5722579ee35643af00ba04
AshHero <57551516+xukefaker@users.noreply.github.com>
2020-08-11 16:40:17 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/blogproject/MainActivity.java
+++ app/src/main/java/com/example/blogproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.blogproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +19,8 @@ private Button button;
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this,"woshizhouyi",Toast.LENGTH_LONG).show();
-
-
-
+                Intent intent = new Intent(MainActivity.this, FakerTest.class);
+                startActivity(intent);
             }
         });
     }
